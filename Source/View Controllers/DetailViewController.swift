72f8272b2b6f2fcdcc0539a1b8494a8a378dc01d
cwalo <cjwalo@gmail.com>
2019-07-31 14:49:47 -0500

    Add DetailViewController

---
+++ Source/View Controllers/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  chaise
+//
+//  Created by Corey Walo on 7/31/19.
+//  Copyright © 2019 Corey Walo. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, Storyboarded {
+
+    var event: EventEntity!
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = event.title
+
+        imageView.image = nil
+        dateLabel.text = String(describing: event.date)
+        locationLabel.text = event.location
+    }
+    
+}
