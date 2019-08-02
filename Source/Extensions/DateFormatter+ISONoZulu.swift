0d96e016c6bfc1fc88d4124c7562ca4590d926e0
cwalo <cjwalo@gmail.com>
2019-08-01 20:32:59 -0500

    “Cache” ISO8601NoZulu DateFormatter

--- Source/Extensions/DateFormatter+ISONoZulu.swift
+++ Source/Extensions/DateFormatter+ISONoZulu.swift
@@ -8,12 +8,19 @@
 
 import Foundation
 
+fileprivate var cachedFormatter: DateFormatter?
+
 extension DateFormatter {
     static func ISO8601NoZulu() -> DateFormatter {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        return formatter
+        if let cachedFormatter = cachedFormatter {
+            return cachedFormatter
+        } else {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+            formatter.timeZone = TimeZone(secondsFromGMT: 0)
+            cachedFormatter = formatter
+            return formatter
+        }
     }
 }
